36d2944c7a5db5eee993b6a47e21222d35992a2c
mayi-oi <arkadlen@proton.me>
2026-01-26 14:04:28 +0100

    recode ganze "main-core" code lol

--- haupt-folder/main-core.cpp
+++ haupt-folder/main-core.cpp
@@ -1,61 +1,43 @@
-// Erstellt von mayi-oi am 12.06.2024
-#include <Wire.h>
-#include <I2Cdev.h>
-#include <MPU6050.h>
-#include <PID_v1.h>
+#define SENDEN 6
+#define ECHO 7
+#define LAUTSPRECHER 4
+#define LED 8
 
-// Create an MPU6050 object
-MPU6050 mpu; //(keine anhung wie habe MPU6500 mpu?)
+long Zeit;
+long Entferung;
 
-// PID control variables
-double Setpoint, Input, Output;
-double Kp = 10, Ki = 5, Kd = 1;
-PID pid(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+void setup() {
+  
+  pinMode(SENDEN, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
 
-//Montor-Pins
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
-
-void setup()
-{
-    Serial.begin(9600);
-    Wire.begin();
-    mpu.initialize();
-
-    Setpoint = 0;
-    pid.SetMode(AUTOMATIC);
-    pin.SetOutputlimts(-255, 255);
-
-    pinMode(enA, OUTPUT);
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
 }
 
-void loop()
-{
-    int16_t ax, ay, az, gx, gy, gz;
-    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+void loop(){
+  
+  digitalWrite(SENDEN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(SENDEN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(SENDEN, LOW);
 
-    double angle = atan2(ay, az) * 180 / PI;
-    Input = angle;
-    pid.Compute();
+Zeit = pulseIn(ECHO, HIGH);
+Entferung = (Zeit / 2) * 0.03432; // in cm
 
-    int motorSpeed = (int)Output;
-    if (Output > 0)
-    {
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-    }
-    else
-    {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        motorSpeed = -motorSpeed;
+// display dinge lol
+Serial.print("Entferung: ");
+Serial.print(Entferung);
+Serial.print(" cm");
 
-        Serial.print("Winkel: ");
-        Serial.print(angle);
-        Serial.print(" Motor: ")
-        Serial.println(Output);
-    }
+if (Entferung > 0 && Entferung < 20) {
+  int frequnez = map(Entferung, 1,20,1000, 200);
+  tone(LAUTSPRECHER, frequnez, 200);
+
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+ }
+  delay(100);
 }
