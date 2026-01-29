017273b58d1c58745d7f936bd663eefa4e9263f9
mayi-oi <arkadlen@proton.me>
2026-01-29 16:35:51 +0100

    recode nochmal lol

--- haupt-folder/idk.cpp
+++ haupt-folder/idk.cpp
@@ -3,58 +3,59 @@
 #include <MPU6050.h>
 #include <PID_v1.h>
 
-// MPU650 mpu;
+MPU6050 mpu;  // Instanz des MPU6050-Sensors
 
+// PID-Variablen
 double Setpoint, Input, Output;
-double Kp = 10, =5, Kd = 1;
-PID pid(&Input,&Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+double Kp = 10, Ki = 5, Kd = 1;  // Ki war falsch deklariert
+PID pid(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
+// Motor-Pins
 int enA = 9;
 int in1 = 8;
 int in2 = 7;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   Wire.begin();
   mpu.initialize();
 
-  Setpoint = 0;
+  Setpoint = 0;  // Zielwinkel (z. B. 0° für Balance)
 
   pid.SetMode(AUTOMATIC);
-  pin.SetOutputlimts(-255, 255);
+  pid.SetOutputLimits(-255, 255); 
 
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
 }
 
-void loop ()
-{
+void loop() {
+  // Sensorwerte lesen
   int16_t ax, ay, az, gx, gy, gz;
-  mpu.getMontion6(&ax, &ay, &az; &gx, &gy, &gz);
-  
-  double angle = atan2(ay, az) * 18ß / PI;
-    Input = angle;
-    pid.Compute();
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);  // Korrigiert: "getMotion6" statt "getMontion6"
 
-    int motorSpeed = (Int)Output;
-    if (Output > 0)
-    {
-      digitalWrite(in1, HIGH);
-      digitalWrite(in2, LOW);
-      {
-        else
-      }
+  // Winkel berechnen (in Grad)
+  double angle = atan2(ay, az) * 180.0 / PI;  // Korrigiert: "180" statt "18ß"
+  Input = angle;
 
-      digitalWrite(in1, LOW);
-      digitalWrite(in2, HIGH);
-      motorSpeed = - motorSpeed;
+  // PID-Regler berechnen
+  pid.Compute();
 
-      // display port
-      Serial.print("Winkel: ");
-      Serial.print(angle);
-      Serial.print("Motor: ");
-      Serial.println(Output);
-    }
+  // Motorsteuerung
+  int motorSpeed = abs((int)Output);  // Betrag für analogWrite
+  if (Output > 0) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  } else {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  }
+  analogWrite(enA, motorSpeed);  // Motorgeschwindigkeit setzen
+
+  // Debugging-Ausgabe
+  Serial.print("Winkel: ");
+  Serial.print(angle);
+  Serial.print("  Motor: ");
+  Serial.println(Output);
 }
